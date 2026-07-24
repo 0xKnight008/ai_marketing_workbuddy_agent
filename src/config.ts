fe36a40eb660a05f3d81dfc0067efe7239d0ab93
0xKnight008 <dev@parastate.io>
2026-07-24 15:47:14 +0800

    chore: retain product and architecture documents only

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-/** Google Form used to collect newsletter sign-ups. */
-export const GOOGLE_FORM_EMBED_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSf0snTCY6aXd-eREWUYHvfHUPsdAxRLiCW2KxJanUQomT0ncA/viewform?embedded=true";
